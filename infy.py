794df0b55fb5fcb30cae5546a528e52d5b290843
Alexios Theodoros Brezas <abresas@gmail.com>
2019-07-17 08:20:04 +0300

    save and load files

--- infy.py
+++ infy.py
@@ -1,6 +1,13 @@
+import os
+
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 from kivy.config import Config
+from kivy.properties import ObjectProperty, ListProperty
+from plyer import filechooser
 
 from grammar import grammar
 from evaluator import Evaluator
@@ -13,10 +20,84 @@ class Line(Widget):
     pass
 
 
-evaluator = Evaluator()
+class LoadButton(Button):
+    '''
+    Button that triggers 'filechooser.open_file()' and processes
+    the data response from filechooser Activity.
+    '''
+
+    selection = ListProperty([])
+
+    def on_press(self):
+        '''
+        Call plyer filechooser API to run a filechooser Activity.
+        '''
+        filechooser.open_file(on_selection=self.handle_selection)
+
+    def handle_selection(self, selection):
+        '''
+        Callback function for handling the selection response from Activity.
+        '''
+        self.selection = selection
+
+    def on_selection(self, *a, **k):
+        '''
+        Update TextInput.text after FileChoose.selection is changed
+        via FileChoose.handle_selection.
+        '''
+        app = App.get_running_app()
+        app.load(self.selection[0])
+
+
+class SaveButton(Button):
+    '''
+    Button that triggers 'filechooser.open_file()' and processes
+    the data response from filechooser Activity.
+    '''
+
+    selection = ListProperty([])
+
+    def on_press(self):
+        '''
+        Call plyer filechooser API to run a filechooser Activity.
+        '''
+        app = App.get_running_app()
+        if app.filepath:
+            app.save(app.filepath)
+        else:
+            filechooser.open_file(on_selection=self.handle_selection)
+
+    def handle_selection(self, selection):
+        '''
+        Callback function for handling the selection response from Activity.
+        '''
+        self.selection = selection
+
+    def on_selection(self, *a, **k):
+        '''
+        Update TextInput.text after FileChoose.selection is changed
+        via FileChoose.handle_selection.
+        '''
+        app = App.get_running_app()
+        app.save(self.selection[0])
 
 
 class InfyApp(App):
+    filepath = None
+
+    def load(self, filepath):
+        with open(filepath) as stream:
+            self.root.textarea.text = stream.read()
+        self.filepath = filepath
+        self.title = 'Infy - ' + os.path.basename(self.filepath)
+
+    def save(self, filepath):
+        print('saving', filepath, self.root.textarea.text)
+        with open(filepath, 'w') as stream:
+            stream.write(self.root.textarea.text)
+        self.filepath = filepath
+        self.title = 'Infy - ' + os.path.basename(self.filepath)
+
     def text_changed(self, txt):
         results_area = self.root.resultsarea
         doc = grammar.parse(txt)
@@ -38,6 +119,8 @@ class InfyApp(App):
         results_area.text = results_text
         print("new text from root.textarea", self.root.textarea.text, results_text)
 
+
+evaluator = Evaluator()
 
 if __name__ == "__main__":
     InfyApp().run()
